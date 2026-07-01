f7823839c73197a8419aaf3f3ec332c41082204b
Logg84 <lorenzetti1984@gmail.com>
2026-07-01 17:12:39 +0200

    app.py

--- app.py
+++ app.py
@@ -1,55 +1,37 @@
 import streamlit as st
+import pandas as pd
 import streamlit.components.v1 as components
 
-# Configurazione pagina
-st.set_page_config(page_title="Monitor Trading Marco", layout="wide")
+st.set_page_config(page_title="Monitor Trading", layout="wide")
 
 st.title("Monitoraggio Asset - Ufficio Logistica")
 
-# Sezione Input
-col1, col2 = st.columns([1, 2])
+# Legge il file CSV che abbiamo creato su GitHub
+try:
+    df = pd.read_csv("watchlist.csv")
+    st.subheader("La tua Watchlist")
+    st.table(df)
+    
+    # Seleziona un ticker dalla lista per vedere il grafico
+    selected_ticker = st.selectbox("Seleziona ticker da visualizzare:", df['ticker'].tolist())
+except:
+    st.error("File watchlist.csv non trovato nel repository!")
+    selected_ticker = "AAPL"
 
-with col1:
-    st.subheader("Aggiungi Livello")
-    ticker = st.text_input("Inserisci Ticker (es. AAPL, EURUSD)").upper()
-    level = st.number_input("Livello di Attenzione", format="%.2f")
-    if st.button("Aggiungi alla lista"):
-        if 'watchlist' not in st.session_state:
-            st.session_state.watchlist = []
-        st.session_state.watchlist.append({"ticker": ticker, "level": level})
-        st.success(f"Aggiunto {ticker} a {level}")
-
-# Sezione Widget TradingView
-with col2:
-    st.subheader("Grafico Live")
-    if ticker:
-        # Widget TradingView ufficiale
-        html_code = f"""
-        <div class="tradingview-widget-container">
-          <div id="tradingview_chart"></div>
-          <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-          <script type="text/javascript">
-          new TradingView.widget({{
-            "width": "100%",
-            "height": 500,
-            "symbol": "{ticker}",
-            "interval": "D",
-            "timezone": "Etc/UTC",
-            "theme": "light",
-            "style": "1",
-            "locale": "it",
-            "toolbar_bg": "#f1f3f6",
-            "enable_publishing": false,
-            "container_id": "tradingview_chart"
-          }});
-          </script>
-        </div>
-        """
-        components.html(html_code, height=550)
-    else:
-        st.info("Inserisci un ticker per visualizzare il grafico")
-
-# Tabella Watchlist
-st.subheader("La tua Watchlist")
-if 'watchlist' in st.session_state:
-    st.table(st.session_state.watchlist)
+# Widget TradingView
+st.subheader(f"Grafico Live: {selected_ticker}")
+html_code = f"""
+<div class="tradingview-widget-container">
+  <div id="tradingview_chart"></div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget({{
+    "width": "100%", "height": 500,
+    "symbol": "{selected_ticker}",
+    "interval": "D", "theme": "light", "locale": "it",
+    "container_id": "tradingview_chart"
+  }});
+  </script>
+</div>
+"""
+components.html(html_code, height=550)
